49577f43762a80018c13bf210fb60828c736f737
Jiuyang Liu <liu@jiuyang.me>
2026-07-20 05:13:10 +0000

    [FIRRTL] Add IR-level declaration comments
    Preserve declaration comments through FIRRTL, HW, Seq, and SV lowering and transformations, and emit them in Verilog.

--- lib/Conversion/FIRRTLToHW/LowerToHW.cpp
+++ lib/Conversion/FIRRTLToHW/LowerToHW.cpp
@@ -881,8 +881,7 @@ void FIRRTLModuleLowering::lowerFileHeader(CircuitOp op,
 
   // Helper function to emit #ifndef guard.
   auto emitGuard = [&](const char *guard, llvm::function_ref<void(void)> body) {
-    sv::IfDefOp::create(
-        b, guard, [] {}, body);
+    sv::IfDefOp::create(b, guard, [] {}, body);
   };
 
   if (state.usedFileDescriptorLib)
@@ -1362,8 +1361,7 @@ FIRRTLModuleLowering::lowerModule(FModuleOp oldModule, Block *topLevelModule,
   auto newModule =
       hw::HWModuleOp::create(builder, oldModule.getLoc(), nameAttr, ports);
 
-  if (auto comment = oldModule->getAttrOfType<StringAttr>("comment"))
-    newModule.setCommentAttr(comment);
+  copyDeclarationComment(oldModule, newModule);
 
   // Copy over any attributes which are not required for FModuleOp.
   SmallVector<StringRef, 13> attrNames = {
@@ -1401,8 +1399,18 @@ FIRRTLModuleLowering::lowerModule(FModuleOp oldModule, Block *topLevelModule,
     if (loweringState.isInTestHarness(oldModule)) {
       if (!newModule->hasAttr("output_file"))
         newModule->setAttr("output_file", testBenchDir);
-      newModule.setCommentAttr(
-          builder.getStringAttr("VCS coverage exclude_file"));
+      newModule->setAttr("firrtl.extract.do_not_extract",
+                         builder.getUnitAttr());
+      constexpr StringLiteral generatedComment = "VCS coverage exclude_file";
+      if (auto sourceComment = newModule.getCommentAttr()) {
+        SmallString<64> combined(sourceComment.getValue());
+        if (!combined.empty())
+          combined += '\n';
+        combined += generatedComment;
+        newModule.setCommentAttr(builder.getStringAttr(combined));
+      } else {
+        newModule.setCommentAttr(builder.getStringAttr(generatedComment));
+      }
     }
 
   if (handleForceNameAnnos(oldModule, annos, loweringState))
@@ -3273,8 +3281,7 @@ void FIRRTLLowering::addToAlwaysBlock(
       auto createIfOp = [&]() {
         // It is weird but intended. Here we want to create an empty sv.if
         // with an else block.
-        insideIfOp = sv::IfOp::create(
-            builder, reset, [] {}, [] {});
+        insideIfOp = sv::IfOp::create(builder, reset, [] {}, [] {});
       };
       if (resetStyle == sv::ResetType::AsyncReset) {
         sv::EventControl events[] = {clockEdge, resetEdge};
@@ -3737,6 +3744,7 @@ LogicalResult FIRRTLLowering::visitDecl(WireOp op) {
 
   if (auto svAttrs = sv::getSVAttributes(op))
     sv::setSVAttributes(wire, svAttrs);
+  copyDeclarationComment(op, wire);
 
   return setLowering(op.getResult(), wire);
 }
@@ -3781,15 +3789,26 @@ LogicalResult FIRRTLLowering::visitDecl(NodeOp op) {
   // that we have a symbol name so we can keep the node as a wire.
   auto name = op.getNameAttr();
   auto innerSym = lowerInnerSymbol(op);
+  bool hasDeclarationCarrier = false;
 
-  if (innerSym)
+  if (innerSym) {
     operand = hw::WireOp::create(builder, operand, name, innerSym);
+    hasDeclarationCarrier = true;
+  }
 
   // Move SV attributes.
   if (auto svAttrs = sv::getSVAttributes(op)) {
-    if (!innerSym)
+    if (!innerSym) {
       operand = hw::WireOp::create(builder, operand, name);
+      hasDeclarationCarrier = true;
+    }
     sv::setSVAttributes(operand.getDefiningOp(), svAttrs);
+  }
+
+  if (hasDeclarationComment(op)) {
+    if (!hasDeclarationCarrier)
+      operand = hw::WireOp::create(builder, operand, name);
+    copyDeclarationComment(op, operand.getDefiningOp());
   }
 
   return setLowering(op.getResult(), operand);
@@ -3849,6 +3868,7 @@ LogicalResult FIRRTLLowering::visitDecl(RegOp op) {
   // Move SV attributes.
   if (auto svAttrs = sv::getSVAttributes(op))
     sv::setSVAttributes(reg, svAttrs);
+  copyDeclarationComment(op, reg);
 
   inputEdge.setValue(reg);
   (void)setLowering(op.getResult(), reg);
@@ -3913,6 +3933,7 @@ LogicalResult FIRRTLLowering::visitDecl(RegResetOp op) {
   // Move SV attributes.
   if (auto svAttrs = sv::getSVAttributes(op))
     sv::setSVAttributes(reg, svAttrs);
+  copyDeclarationComment(op, reg);
 
   inputEdge.setValue(reg);
   (void)setLowering(op.getResult(), reg);
@@ -4162,6 +4183,7 @@ LogicalResult FIRRTLLowering::visitDecl(InstanceOp oldInstance) {
   auto newInstance =
       hw::InstanceOp::create(builder, newModule, oldInstance.getNameAttr(),
                              operands, parameters, innerSym);
+  copyDeclarationComment(oldInstance, newInstance);
 
   if (oldInstance.getLowerToBind() || oldInstance.getDoNotPrint())
     newInstance.setDoNotPrintAttr(builder.getUnitAttr());
